66302a1cc3d0089319c2749d0f69b086d360810a
Santiago Sica <santisica29@gmail.com>
2024-08-17 15:41:00 -0300

    Fixed namespace

--- Calculator/Helpers.cs
+++ Calculator/Helpers.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace CalculatorHelpers;
+namespace CalculatorProgram;
 
 internal class Helpers
 {
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.RegularExpressions;
-using CalculatorHelpers;
 using CalculatorLibrary;
 
 namespace CalculatorProgram;
